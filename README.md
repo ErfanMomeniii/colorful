105d6e3ee15d1f9289aa5b1d982b3a248c87071c
Erfan Momeni <Erfan.Momeni@snapp.cab>
2022-12-26 01:21:38 +0330

    update readme

--- README.md
+++ README.md
@@ -2,4 +2,39 @@
 
 # colorful
 
-colorful is a lightweight package for rendering colorful text in terminal with Go
+colorful is a lightweight package for rendering colorful text in terminal with Go you can use this instead of **fmt** package for having many features.
+
+
+# Documentation
+## Install
+```bash
+go get github.com/ErfanMomeniii/colorful
+```   
+Next, include it in your application:
+```bash
+import "github.com/ErfanMomeniii/colorful"
+``` 
+### Supported Colors
+
+|                                                                 |
+|:----------------------------------------------------------------| 
+| ![black color](./assets/photo/colors/Black.png)                 |
+| ![red color](./assets/photo/colors/Red.png)                     |
+| ![green color](./assets/photo/colors/Green.png)                 |
+| ![yellow color](./assets/photo/colors/Yellow.png)               |
+| ![blue color](./assets/photo/colors/Blue.png)                   |
+| ![magenta color](./assets/photo/colors/Magenta.png)             |
+| ![cyan color](./assets/photo/colors/Cyan.png)                   |
+| ![white color](./assets/photo/colors/White.png)                 |
+| ![brightBlack color](./assets/photo/colors/BrightBlack.png)     |
+| ![brightRed color](./assets/photo/colors/BrightRed.png)         |
+| ![brightGreen color](./assets/photo/colors/BrightGreen.png)     |
+| ![brightYellow color](./assets/photo/colors/BrightYellow.png)   |
+| ![brightBlue color](./assets/photo/colors/BrightBlue.png)       |
+| ![brightMagenta color](./assets/photo/colors/BrightMagenta.png) |
+| ![brightCyan color](./assets/photo/colors/BrightCyan.png)       |
+| ![brightWhite color](./assets/photo/colors/BrightWhite.png)     |
+
+
+## Quick Start
+
--- example/example.go
+++ example/example.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"github.com/ErfanMomeniii/colorful"
 )
 
 func main() {
-
-	fmt.Print(colorful.Sprint(colorful.BlackColor, colorful.BlueBackground, " success "))
-	colorful.Print(colorful.BlackColor, colorful.GreenBackground, " success ")
+	colorful.Println(colorful.BlackColor, colorful.DefaultBackground, "BlackColor", colorful.ResetColor)
+	colorful.Println(colorful.RedColor, colorful.DefaultBackground, "RedColor", colorful.ResetColor)
+	colorful.Println(colorful.GreenColor, colorful.DefaultBackground, "GreenColor", colorful.ResetColor)
+	colorful.Println(colorful.YellowColor, colorful.DefaultBackground, "YellowColor", colorful.ResetColor)
+	colorful.Println(colorful.BlueColor, colorful.DefaultBackground, "BlueColor", colorful.ResetColor)
+	colorful.Println(colorful.MagentaColor, colorful.DefaultColor, "MagentaColor", colorful.ResetColor)
+	colorful.Println(colorful.CyanColor, colorful.DefaultBackground, "CyanColor", colorful.ResetColor)
+	colorful.Println(colorful.WhiteColor, colorful.DefaultBackground, "WhiteColor", colorful.ResetColor)
+	colorful.Println(colorful.BrightBlackColor, colorful.DefaultBackground, "BrightBlackColor", colorful.ResetColor)
+	colorful.Println(colorful.BrightRedColor, colorful.DefaultBackground, "BrightRedColor", colorful.ResetColor)
+	colorful.Println(colorful.BrightGreenColor, colorful.DefaultBackground, "BrightGreenColor", colorful.ResetColor)
+	colorful.Println(colorful.BrightYellowColor, colorful.DefaultColor, "BrightYellowColor", colorful.ResetColor)
+	colorful.Println(colorful.BrightBlueColor, colorful.DefaultColor, "BrightBlueColor", colorful.ResetColor)
+	colorful.Println(colorful.BrightMagentaColor, colorful.DefaultColor, "BrightMagentaColor", colorful.ResetColor)
+	colorful.Println(colorful.BrightCyanColor, colorful.DefaultColor, "BrightCyanColor", colorful.ResetColor)
+	colorful.Println(colorful.BrightWhiteColor, colorful.DefaultColor, "BrightWhiteColor", colorful.ResetColor)
 }
