84d0cd12f580b81a8ddb3617bf5579d5d70a9bc8
Erfan Momeni <Erfan.Momeni@snapp.cab>
2023-06-25 15:43:08 +0330

    fix: update

--- color_test.go
+++ color_test.go
@@ -7,129 +7,129 @@ import (
 
 func TestSprint(t *testing.T) {
 	cases := []struct {
-		name          string
-		expectedValue string
-		text          string
-		color         string
-		background    string
+		name            string
+		expectedValue   string
+		text            string
+		color           string
+		backgroundColor string
 	}{
 		{
-			name:          "check with default color and background",
-			expectedValue: "hello" + ResetColor,
-			text:          "hello",
-			color:         DefaultColor,
-			background:    DefaultBackground,
+			name:            "check with default color and background",
+			expectedValue:   "hello" + ResetColor,
+			text:            "hello",
+			color:           DefaultColor,
+			backgroundColor: DefaultBackgroundColor,
 		}, {
-			name:          "check with not default color and not default background",
-			expectedValue: RedColor + BlueBackground + "hello" + ResetColor,
-			text:          "hello",
-			color:         RedColor,
-			background:    BlueBackground,
+			name:            "check with not default color and not default background",
+			expectedValue:   RedColor + BlueBackgroundColor + "hello" + ResetColor,
+			text:            "hello",
+			color:           RedColor,
+			backgroundColor: BlueBackgroundColor,
 		},
 		{
-			name:          "check with default color and not default background",
-			expectedValue: DefaultColor + RedBackground + "hello" + ResetColor,
-			text:          "hello",
-			color:         DefaultColor,
-			background:    RedBackground,
+			name:            "check with default color and not default background",
+			expectedValue:   DefaultColor + RedBackgroundColor + "hello" + ResetColor,
+			text:            "hello",
+			color:           DefaultColor,
+			backgroundColor: RedBackgroundColor,
 		},
 		{
-			name:          "check with not default color and default background",
-			expectedValue: RedColor + DefaultBackground + "hello" + ResetColor,
-			text:          "hello",
-			color:         RedColor,
-			background:    DefaultBackground,
+			name:            "check with not default color and default background",
+			expectedValue:   RedColor + DefaultBackgroundColor + "hello" + ResetColor,
+			text:            "hello",
+			color:           RedColor,
+			backgroundColor: DefaultBackgroundColor,
 		},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			require.Equal(t, c.expectedValue, Sprint(c.color, c.background, c.text))
+			require.Equal(t, c.expectedValue, WithColor(c.color).WithBackgroundColor(c.backgroundColor).Sprint(c.text))
 		})
 	}
 }
 
 func TestSprintln(t *testing.T) {
 	cases := []struct {
-		name          string
-		expectedValue string
-		text          string
-		color         string
-		background    string
+		name            string
+		expectedValue   string
+		text            string
+		color           string
+		backgroundColor string
 	}{
 		{
-			name:          "check with default color and background",
-			expectedValue: "hello" + ResetColor + "\n",
-			text:          "hello",
-			color:         DefaultColor,
-			background:    DefaultBackground,
+			name:            "check with default color and background",
+			expectedValue:   "hello" + ResetColor + "\n",
+			text:            "hello",
+			color:           DefaultColor,
+			backgroundColor: DefaultBackgroundColor,
 		}, {
-			name:          "check with not default color and not default background",
-			expectedValue: RedColor + BlueBackground + "hello" + ResetColor + "\n",
-			text:          "hello",
-			color:         RedColor,
-			background:    BlueBackground,
+			name:            "check with not default color and not default background",
+			expectedValue:   RedColor + BlueBackgroundColor + "hello" + ResetColor + "\n",
+			text:            "hello",
+			color:           RedColor,
+			backgroundColor: BlueBackgroundColor,
 		},
 		{
-			name:          "check with default color and not default background",
-			expectedValue: DefaultColor + RedBackground + "hello" + ResetColor + "\n",
-			text:          "hello",
-			color:         DefaultColor,
-			background:    RedBackground,
+			name:            "check with default color and not default background",
+			expectedValue:   DefaultColor + RedBackgroundColor + "hello" + ResetColor + "\n",
+			text:            "hello",
+			color:           DefaultColor,
+			backgroundColor: RedBackgroundColor,
 		},
 		{
-			name:          "check with not default color and default background",
-			expectedValue: RedColor + DefaultBackground + "hello" + ResetColor + "\n",
-			text:          "hello",
-			color:         RedColor,
-			background:    DefaultBackground,
+			name:            "check with not default color and default background",
+			expectedValue:   RedColor + DefaultBackgroundColor + "hello" + ResetColor + "\n",
+			text:            "hello",
+			color:           RedColor,
+			backgroundColor: DefaultBackgroundColor,
 		},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			require.Equal(t, c.expectedValue, Sprintln(c.color, c.background, c.text))
+			require.Equal(t, c.expectedValue, WithColor(c.color).WithBackgroundColor(c.backgroundColor).Sprintln(c.text))
 		})
 	}
 }
 
 func TestSprintf(t *testing.T) {
 	cases := []struct {
-		name          string
-		expectedValue string
-		format        string
-		parameter     any
-		color         string
-		background    string
+		name            string
+		expectedValue   string
+		format          string
+		parameter       any
+		color           string
+		backgroundColor string
 	}{
 		{
-			name:          "check with default color and background",
-			expectedValue: "hello" + ResetColor,
-			format:        "%s",
-			parameter:     "hello",
-			color:         DefaultColor,
-			background:    DefaultBackground,
+			name:            "check with default color and background",
+			expectedValue:   "hello" + ResetColor,
+			format:          "%s",
+			parameter:       "hello",
+			color:           DefaultColor,
+			backgroundColor: DefaultBackgroundColor,
 		}, {
-			name:          "check with not default color and not default background",
-			expectedValue: RedColor + BlueBackground + "2" + ResetColor,
-			format:        "%d",
-			parameter:     2,
-			color:         RedColor,
-			background:    BlueBackground,
+			name:            "check with not default color and not default background",
+			expectedValue:   RedColor + BlueBackgroundColor + "2" + ResetColor,
+			format:          "%d",
+			parameter:       2,
+			color:           RedColor,
+			backgroundColor: BlueBackgroundColor,
 		},
 		{
-			name:          "check with default color and not default background",
-			expectedValue: DefaultColor + RedBackground + "3.14" + ResetColor,
-			format:        "%1.2f",
-			parameter:     3.14,
-			color:         DefaultColor,
-			background:    RedBackground,
+			name:            "check with default color and not default background",
+			expectedValue:   DefaultColor + RedBackgroundColor + "3.14" + ResetColor,
+			format:          "%1.2f",
+			parameter:       3.14,
+			color:           DefaultColor,
+			backgroundColor: RedBackgroundColor,
 		},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			require.Equal(t, c.expectedValue, Sprintf(c.color, c.background, c.format, c.parameter))
+			require.Equal(t, c.expectedValue, WithColor(c.color).WithBackgroundColor(c.backgroundColor).Sprintf(c.format, c.parameter))
 		})
 	}
 }
